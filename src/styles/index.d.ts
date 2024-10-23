6f130e1fb2861379a00f60630ac4fe816d1348af
cla6shade <cla6shade@gmail.com>
2024-10-23 18:25:08 +0900

    feat: default color maps

---
+++ src/styles/index.d.ts
@@ -0,0 +1,62 @@
+import { CSSObject } from '@emotion/react';
+import { breakPoints } from '@styles/breakpoints';
+
+export type ButtonVariants = 'default' | 'dark' | 'primary';
+
+export type TagVariants = 'default' | 'primary';
+
+export type Colors = {
+  primary: {
+    main: string;
+    lighten: string;
+    darken: string;
+    passive: string;
+  };
+  text: {
+    prominent: string;
+    moderate: string;
+    subtle: string;
+  };
+  background: {
+    main: string;
+    lighten: string;
+    darken: string;
+  };
+  border: {
+    subtle: string;
+    prominent: string;
+  };
+  absolute: {
+    black: string;
+    white: string;
+  };
+  other: {
+    link: string;
+    success: string;
+    warn: string;
+    error: string;
+  };
+  brand: {
+    background: string;
+    text: string;
+    primary: string;
+  };
+};
+
+export type Corners = {
+  small: string;
+  medium: string;
+  big: string;
+  round: string;
+};
+
+export type BreakpointKey = keyof typeof breakPoints;
+
+export type ResponsiveStyle = Partial<Record<BreakpointKey, CSSObject>>;
+
+declare module '@emotion/react' {
+  export interface Theme {
+    colors: Colors;
+    corners: Corners;
+  }
+}
