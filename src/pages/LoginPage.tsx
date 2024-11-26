aa771c86f929222a59ef8a4679d8635c69711b77
harugi7 <gimme2721@gmail.com>
2024-11-26 14:19:27 +0900

    feat: 로그인 페이지 구현

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,28 +2,38 @@ import { DefaultPaddedContainer } from '@components/container/variants';
 import Container from '@components/container';
 import { Heading, Paragraph } from '@components/text';
 import { useTheme } from '@emotion/react';
+import Button from '@components/button';
+import logo from '@/assets/bulning-logo.svg';
 
 function LoginPage() {
   const theme = useTheme();
+  const kakaoLoginHandler = () => {
+    window.location.href = `${import.meta.env.VITE_BASE_URL}/login/kakao`;
+  };
   return (
     <DefaultPaddedContainer>
-      <Container height="100%" direction="column" justify="space-between" css={{ minHeight: '700px' }}>
-        <Container direction="column" align="flex-start" css={{ marginTop: '89px' }}>
+      <Container height="100dvh" direction="column" justify="space-between" css={{ paddingBottom: '61px', paddingTop: '89px' }}>
+        <Container direction="column" align="flex-start">
           <Heading.H3 css={{ fontWeight: '600', lineHeight: '38px' }}>우리 동네 벌레잡이 매칭 서비스</Heading.H3>
-          <Heading.H2 css={{ fontWeight: '600', lineHeight: '38px' }}>BULNING</Heading.H2>
-          <Container justify="flex-start" align="flex-start" css={{ fontSize: '72px' }}>🐛</Container>
+          <Heading.H2 css={{ fontWeight: '600', lineHeight: '45px' }}>BULNING</Heading.H2>
+          <img src={logo} alt="logo" width="72px" />
         </Container>
         <Container direction="column">
           <Paragraph variant="small" color={theme.colors.text.moderate}>SNS 계정으로 간편 가입하기</Paragraph>
-          <Container
-            height="50px"
-            width="100%"
+          <Button
+            onClick={kakaoLoginHandler}
             css={{
-              borderRadius: '8px', backgroundColor: '#FEE500', fontSize: '18px', marginTop: '15px',
+              height: '50px',
+              width: '100%',
+              borderRadius: '8px',
+              backgroundColor: '#FEE500',
+              fontSize: '18px',
+              marginTop: '15px',
+              border: 'none',
             }}
           >
             카카오톡으로 시작하기
-          </Container>
+          </Button>
         </Container>
       </Container>
     </DefaultPaddedContainer>
