f56bf1233d066475babe5bd890548d70eb260a3f
harugi7 <gimme2721@gmail.com>
2024-11-26 14:19:53 +0900

    chore: main에 Provider 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import { UserProvider } from '@providers/UserProvider.tsx';
+import { ThemeProvider } from '@emotion/react';
+import theme from '@styles/theme';
+import BrowserRouterProvider from '@providers/BrowserRouterProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
+
   <StrictMode>
-    <App />
+    <UserProvider>
+      <ThemeProvider theme={theme}>
+        <BrowserRouterProvider />
+      </ThemeProvider>
+    </UserProvider>
   </StrictMode>,
 );
