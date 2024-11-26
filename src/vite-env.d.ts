9ce1eb924a8ee076e83b98b62e888e44d9b01d73
harugi7 <gimme2721@gmail.com>
2024-11-26 14:22:22 +0900

    chore: base url을 위한 설정

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,12 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+
+import 'axios';
+
+interface ImportMetaEnv {
+  readonly VITE_BASE_URL: string;
+
+}
+interface ImportMeta {
+  env: ImportMetaEnv;
+}
