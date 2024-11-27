53ebe84243a9d9e2a833fc2081151aef71c58632
harugi7 <gimme2721@gmail.com>
2024-11-27 15:39:06 +0900

    chore: MainPage CatchRequestList 목데이터 수정

--- src/mock/request/index.ts
+++ src/mock/request/index.ts
@@ -1,6 +1,7 @@
-import logo from '@assets/icons/alarm.svg';
+import logo from '@assets/icons/x.svg';
+import { CatchRequest } from '@/types/request';
 
-export const mockRequestList = [
+export const mockRequestList: CatchRequest[] = [
   {
     id: 1,
     title: '용병 구함',
@@ -8,61 +9,61 @@ export const mockRequestList = [
     how_far: '100m',
     how_long: '10분 전',
     price: '5000',
-    image_url: { logo },
+    image_url: logo,
   },
   {
     id: 2,
-    title: '용병 구함',
+    title: '긴급! 당신의 도움이 필요합니다!',
     location: '금정구 장전1동',
     how_far: '100m',
     how_long: '10분 전',
     price: '5000',
-    image_url: { logo },
+    image_url: logo,
   },
   {
     id: 3,
-    title: '용병 구함',
+    title: '오크 무리의 습격을 막아주세요.',
     location: '금정구 장전1동',
     how_far: '100m',
     how_long: '10분 전',
     price: '5000',
-    image_url: { logo },
+    image_url: logo,
   },
   {
     id: 4,
-    title: '용병 구함',
+    title: '검술과 활술에 능한 자.',
     location: '금정구 장전1동',
     how_far: '100m',
     how_long: '10분 전',
     price: '5000',
-    image_url: { logo },
+    image_url: logo,
   },
   {
     id: 5,
-    title: '용병 구함',
+    title: '마법사가 반드시 포함되어야 합니다.',
     location: '금정구 장전1동',
     how_far: '100m',
     how_long: '10분 전',
     price: '5000',
-    image_url: { logo },
+    image_url: logo,
   },
   {
     id: 6,
-    title: '용병 구함',
+    title: '용맹하고 신뢰할 수 있는 분만 지원해주세요.',
     location: '금정구 장전1동',
     how_far: '100m',
     how_long: '10분 전',
     price: '5000',
-    image_url: { logo },
+    image_url: logo,
   },
   {
     id: 7,
-    title: '용병 구함',
+    title: '황금 500코인 지급.',
     location: '금정구 장전1동',
     how_far: '100m',
     how_long: '10분 전',
     price: '5000',
-    image_url: { logo },
+    image_url: logo,
   },
 
 ];
