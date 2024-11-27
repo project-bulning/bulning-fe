c7a19c6902a5fcedf47557db1285295266a5bd43
harugi7 <gimme2721@gmail.com>
2024-11-27 14:06:54 +0900

    chore: Request 타입

---
+++ src/types/request/index.d.ts
@@ -0,0 +1,11 @@
+export interface Request {
+  id: number;
+  title: string;
+  location: string;
+  how_far: string;
+  how_long: string;
+  price: string;
+  image_url: string;
+}
+
+export type RequestDetail = Request;
