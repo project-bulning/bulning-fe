516f643e21790007e2718300f5e9529a2fcf6db5
harugi7 <gimme2721@gmail.com>
2024-11-27 14:27:56 +0900

    chore: MainPage router 추가

--- src/providers/BrowserRouterProvider.tsx
+++ src/providers/BrowserRouterProvider.tsx
@@ -2,8 +2,15 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import routePaths from '@constants/routePaths.ts';
 import UserProfilePage from '@pages/LoginAuthPage.tsx';
 import LoginPage from '@pages/LoginPage.tsx';
+import MainPage from '@pages/MainPage.tsx';
 
 const router = createBrowserRouter([
+  {
+    path: routePaths.MAIN,
+    element: (
+      <MainPage />
+    ),
+  },
   {
     path: routePaths.LOGIN,
     element: (
