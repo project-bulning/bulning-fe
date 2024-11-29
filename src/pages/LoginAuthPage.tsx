7fafb91ade11d5a75ddbf9e66ac738d5167b87ba
harugi7 <gimme2721@gmail.com>
2024-11-29 20:09:24 +0900

    feat: token 가져오기

--- src/pages/LoginAuthPage.tsx
+++ src/pages/LoginAuthPage.tsx
@@ -1,46 +1,24 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import routePaths from '@constants/routePaths';
 
 function LoginAuthPage() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const accessToken = urlParams.get('access_token');
+    const url = window.location.href;
+    const params = new URLSearchParams(new URL(url).search);
+
+    const accessToken = params.get('access_token');
 
     if (accessToken) {
-      fetchUserInfo(accessToken);
+      localStorage.setItem('access_token', accessToken);
+      console.log('Access token saved:', accessToken);
+      navigate(routePaths.MAIN);
     } else {
-      console.log('accessToken not set');
-      navigate('/login');
+      console.log('Access token not found in the URL.');
     }
   }, [navigate]);
-
-  const fetchUserInfo = async (token: string) => {
-    try {
-      // 백엔드 API를 통해 사용자 정보 요청
-      const response = await axios.get('/auth/login', {
-        headers: {
-          Authorization: `Bearer ${token}`, // 토큰을 Authorization 헤더에 포함
-        },
-      });
-
-      // User 정보를 처리
-      const userInfo = response.data;
-      console.log('User Info:', userInfo);
-
-      // 예: Redux 상태나 Context에 사용자 정보 저장
-      // dispatch(setUserInfo(userInfo));
-
-      // 메인 페이지로 이동
-      navigate('/');
-    } catch (error) {
-      console.error('Error fetching user info:', error);
-      // 오류 발생 시 로그인 페이지로 리디렉션
-      navigate('/login');
-    }
-  };
 
   return null;
 }
