c7942e00ebecc07e1517fe054a00ec721f7edb25
harugi7 <gimme2721@gmail.com>
2024-12-02 15:20:02 +0900

    feat: bottomSheet의 handleBar 여부 props로 받도록 추가

--- src/components/bottomSheet/index.tsx
+++ src/components/bottomSheet/index.tsx
@@ -5,6 +5,7 @@ import { CSSObject } from '@emotion/react';
 interface BottomSheetProps {
   isOpen: boolean;
   onChange?: (isOpen: boolean) => void;
+  handleBar?: boolean;
   children: React.ReactNode;
   css?: CSSObject;
 }
@@ -17,7 +18,7 @@ export const fadeOutAnimationState = {
 };
 
 function BottomSheet({
-  isOpen, onChange, children, css,
+  isOpen, onChange, handleBar = true, children, css,
 }: BottomSheetProps) {
   const {
     modalStyle,
@@ -59,7 +60,7 @@ function BottomSheet({
       />
       <div css={modalStyle(animationState === fadeOutAnimationState.FADING)}>
         <div css={bottomHeaderStyle}>
-          <div css={handleBarStyle} />
+          {handleBar && <div css={handleBarStyle} />}
         </div>
         <div css={contentWrapperStyle}>
           <div css={contentStyle}>{children}</div>
