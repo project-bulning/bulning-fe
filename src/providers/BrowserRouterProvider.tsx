46d439b7ee9157b67ac1b6596c13ff5b2be52f1d
harugi7 <gimme2721@gmail.com>
2025-01-05 19:28:07 +0900

    chore: login page들 login 폴더 안으로 이동

--- src/providers/BrowserRouterProvider.tsx
+++ src/providers/BrowserRouterProvider.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import routePaths from '@constants/routePaths.ts';
-import LoginPage from '@pages/login/LoginPage.tsx';
+import LoginPage from '@pages/login/LoginPage';
 import MainPage from '@pages/MainPage.tsx';
-import LoginAuthPage from '@pages/login/LoginAuthPage.tsx';
+import LoginAuthPage from '@pages/login/LoginAuthPage';
 
 const router = createBrowserRouter([
   {
