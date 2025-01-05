ba28fb59a78b2f076188517524520daf494ddf00
mini-apple <suc2150@gmail.com>
2025-01-05 20:25:34 +0900

    feat: AH-01, 임시 채팅페이지 바텀시트 연결

---
+++ src/pages/afterHunting/TempChatPage.stories.tsx
@@ -0,0 +1,23 @@
+import { Meta, StoryObj } from '@storybook/react';
+import TempChatPage from '@pages/afterHunting/TempChatPage';
+import { MemoryRouter } from 'react-router-dom';
+
+const meta: Meta<typeof TempChatPage> = {
+  title: 'Pages/AfterHunt/TempChatPage',
+  component: TempChatPage,
+  decorators: [
+    (Story) => (
+      <MemoryRouter initialEntries={['/']}>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof TempChatPage>;
+
+export const TempChatPageDefault: Story = {
+  args: { },
+};
