a14bf74fe255b62f14f32c4b476d4b72c507e1b7
harugi7 <gimme2721@gmail.com>
2025-01-07 21:10:02 +0900

    chore: 로그인 확인용

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,13 +1,32 @@
-import { DefaultPaddedContainer } from '@components/container/variants';
-import Container from '@components/container';
+import { useEffect } from 'react';
+import { useCurrentUser } from '@providers/CurrentUserProvider';
 
 function MainPage() {
+  const { isLoggedIn, currentUser } = useCurrentUser();
+
+  useEffect(() => {
+    console.log('isLoggedIn:', isLoggedIn);
+    console.log('currentUser:', currentUser);
+  }, [isLoggedIn, currentUser]);
+
   return (
-    <DefaultPaddedContainer>
-      <Container>
-        MainPage
-      </Container>
-    </DefaultPaddedContainer>
+    <div>
+      {isLoggedIn ? (
+        <div>
+          <p>로그인되었습니다!</p>
+          <p>
+            이름:
+            {currentUser?.name}
+          </p>
+          <p>
+            이메일:
+            {currentUser?.email}
+          </p>
+        </div>
+      ) : (
+        <p>로그인되지 않았습니다.</p>
+      )}
+    </div>
   );
 }
 
