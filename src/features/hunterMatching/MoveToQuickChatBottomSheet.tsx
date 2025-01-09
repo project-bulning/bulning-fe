cd3d889672326b99ef58aa52c584d3fc25f517ba
harugi7 <gimme2721@gmail.com>
2025-01-09 17:16:10 +0900

    feat: move to quickchat bottom sheet

--- src/features/hunterMatching/MoveToQuickChatBottomSheet.tsx
+++ src/features/hunterMatching/MoveToQuickChatBottomSheet.tsx
@@ -10,17 +10,19 @@ interface MoveToQuickChatBottomSheetProps {
 
 function MoveToQuickChatBottomSheet({ isOpen, onClose }: MoveToQuickChatBottomSheetProps) {
   return (
-    <BottomSheet isOpen={isOpen} onChange={onClose} hideHandleBar>
+    <BottomSheet isOpen={isOpen} onChange={onClose}>
       <Container
         direction="column"
         justify="flex-start"
         align="flex-start"
         gap="15px"
       >
-        <Heading.H3 weight="bold">퀵챗으로 이동할게요</Heading.H3>
-        <Paragraph variant="medium">헌터와 1대1로 대화하며</Paragraph>
-        <Paragraph variant="medium">빠르게 도움을 요청하세요</Paragraph>
-        <Container direction="column" gap="6px">
+        <Heading.H3_5 weight="bold">퀵챗으로 이동할게요</Heading.H3_5>
+        <Container direction="column" padding="0 0 19px 0" gap="5px">
+          <Paragraph variant="medium" weight="regular">헌터와 1대1로 대화하며</Paragraph>
+          <Paragraph variant="medium" weight="regular">빠르게 도움을 요청하세요</Paragraph>
+        </Container>
+        <Container direction="column">
           <Button>대화하러 가기</Button>
         </Container>
       </Container>
