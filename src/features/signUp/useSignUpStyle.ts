94643c44cbd57c087fcd873a46a4edd001ec938e
mini-apple <suc2150@gmail.com>
2025-01-13 01:39:30 +0900

    feat: 하위 상세 약관 바텀시트 구현

--- src/features/signUp/useSignUpStyle.ts
+++ src/features/signUp/useSignUpStyle.ts
@@ -6,13 +6,29 @@ function useSignUpStyle() {
       border-bottom: 1px solid rgba(116, 115, 115, 0.4);
   `;
 
+  const detailStyle = css`
+      cursor: pointer;
+  `;
+
+  const closeBottomSheetStyle = css`
+      padding-bottom: 20px;
+      border-bottom: 1px solid rgba(116, 115, 115, 0.4);
+  `;
+
+  const closeTextStyle = css`
+      cursor: pointer;
+  `;
+
   const scrollContainerStyle = css`
-  max-height: 70vh;
-  overflow-y: auto;
+      max-height: 70vh;
+      overflow-y: auto;
 `;
 
   return {
     allCheckStyle,
+    detailStyle,
+    closeBottomSheetStyle,
+    closeTextStyle,
     scrollContainerStyle,
   };
 }
