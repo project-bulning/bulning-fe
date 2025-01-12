047bca521418adcc81e56bdf65591c3d1a09b6fd
mini-apple <suc2150@gmail.com>
2025-01-12 23:02:29 +0900

    chore: CheckBox 이름 수정

--- src/components/checkbox/index.tsx
+++ src/components/checkbox/index.tsx
@@ -2,14 +2,14 @@ import { InputHTMLAttributes } from 'react';
 import { CSSObject } from '@emotion/react';
 import useCheckboxStyle from '@components/checkbox/useCheckboxStyle';
 
-interface RadioProps extends InputHTMLAttributes<HTMLInputElement> {
+interface CheckBoxProps extends InputHTMLAttributes<HTMLInputElement> {
   type?: 'checkbox';
   css?: CSSObject;
   defaultChecked?: boolean;
   checked?: boolean;
 }
 
-function Radio({ type = 'checkbox', ...rest }: RadioProps) {
+function CheckBox({ type = 'checkbox', ...rest }: CheckBoxProps) {
   const { checkboxStyle } = useCheckboxStyle();
 
   return (
@@ -17,4 +17,4 @@ function Radio({ type = 'checkbox', ...rest }: RadioProps) {
   );
 }
 
-export default Radio;
+export default CheckBox;
