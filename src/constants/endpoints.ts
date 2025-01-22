eb9bd77130cfec11263f644b2e8d9acaacda8627
harugi7 <gimme2721@gmail.com>
2025-01-22 13:41:07 +0900

    feat: add endpoints

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -2,6 +2,8 @@ const prefix = '/api';
 
 const endpoints = {
   myInfo: `${prefix}/my-info`,
+  bugReports: `${prefix}/bug-reports`,
+  submitPersonalInfo: `${prefix}/user/info`,
 };
 
 export default endpoints;
