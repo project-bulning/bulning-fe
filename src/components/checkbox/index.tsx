11ce382fe7b5aa4cb922e7a769f4b384f4c21a03
harugi7 <gimme2721@gmail.com>
2025-01-27 21:04:20 +0900

    feat: labeling checkbox

--- src/components/checkbox/index.tsx
+++ src/components/checkbox/index.tsx
@@ -1,19 +1,34 @@
 import { InputHTMLAttributes } from 'react';
 import { CSSObject } from '@emotion/react';
 import useCheckboxStyle from '@components/checkbox/useCheckboxStyle';
+import { Paragraph } from '@components/text';
 
 interface CheckBoxProps extends InputHTMLAttributes<HTMLInputElement> {
   type?: 'checkbox';
   css?: CSSObject;
   defaultChecked?: boolean;
   checked?: boolean;
+  id: string;
+  labeling: string;
 }
 
-function CheckBox({ type = 'checkbox', ...rest }: CheckBoxProps) {
+function CheckBox({
+  type = 'checkbox', id, labeling, ...rest
+}: CheckBoxProps) {
   const { checkboxStyle } = useCheckboxStyle();
 
   return (
-    <input type={type} css={checkboxStyle} {...rest} />
+    <>
+      <input
+        type={type}
+        id={id}
+        css={checkboxStyle}
+        {...rest}
+      />
+      <label htmlFor={id}>
+        <Paragraph variant="small">{labeling}</Paragraph>
+      </label>
+    </>
   );
 }
 
