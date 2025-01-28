0ea75758f4a2fd109fd0629137f74426fa975815
mini-apple <suc2150@gmail.com>
2025-01-29 00:43:43 +0900

    chore: serviceworker ts js 변환

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,20 @@ import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react({jsxImportSource: "@emotion/react"}), tsconfigPaths()],
-})
+  plugins: [
+    svgr(),
+    react({ jsxImportSource: '@emotion/react' }),
+    tsconfigPaths(),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        // main 앱
+        main: "index.html",
+        // Service Worker
+        "firebase-messaging-sw": "src/utils/firebase/firebase-messaging-sw.ts",
+      },
+    },
+  },
+});
