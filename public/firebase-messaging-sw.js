8a5ca4dfa2bb05c466ff8525d0182df685dac4ff
mini-apple <suc2150@gmail.com>
2025-01-31 00:40:39 +0900

    chore: yarn ci 에러 수정

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,4 +1,3 @@
-// Import Firebase messaging library
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
 
