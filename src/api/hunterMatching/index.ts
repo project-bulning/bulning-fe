1e8d3da563e00308d7ea48e6056ea3a8f6552f28
mini-apple <suc2150@gmail.com>
2025-02-05 16:08:18 +0900

    feat: 헌터사진 api 함수

--- src/api/hunterMatching/index.ts
+++ src/api/hunterMatching/index.ts
@@ -22,3 +22,34 @@ export async function updateMatchStatus(matchId: number, accept: boolean): Promi
     },
   });
 }
+
+export async function uploadHunterImage(image: File): Promise<string> {
+  const formData = new FormData();
+  formData.append('image', image);
+
+  const url = `${endpoints.uploadHunterImage}`;
+  const response = await axiosInstance.post<{ image_url: string }>(
+    url,
+    formData,
+    {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    },
+  );
+
+  return response.data.image_url;
+}
+
+export async function saveHunterImage(imageUrl: string): Promise<void> {
+  const url = `${endpoints.submitHunterImage}`;
+  await axiosInstance.post(
+    url,
+    { img_url: imageUrl },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+  );
+}
