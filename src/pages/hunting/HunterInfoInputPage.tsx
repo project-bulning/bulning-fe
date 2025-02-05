d95f542bb533b8e2beb293d0db580bc60bbce0f4
harugi7 <gimme2721@gmail.com>
2025-02-05 15:05:29 +0900

    feat: HunterInfoInputPage 조건 수정

--- src/pages/hunting/HunterInfoInputPage.tsx
+++ src/pages/hunting/HunterInfoInputPage.tsx
@@ -87,7 +87,7 @@ function HunterInfoInputPage() {
     };
 
     fetchLocation();
-  }, []);
+  }, [setValue]);
 
   const validations = {
     gender: { required: { value: true, message: '성별을 알려주세요.' } },
