6fbda3b411f4ef2a36a9b48c45ee010f5009a499
mini-apple <suc2150@gmail.com>
2025-02-05 21:24:31 +0900

    Revert "refactor: fetch를 이전 단계에서 수행"
    This reverts commit e484f531b31e95f21f734d32ce3fb27748d489f6.

--- src/features/main/CatchRequestList.tsx
+++ src/features/main/CatchRequestList.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { Heading } from '@components/text';
 import Container from '@components/container';
 import Grid from '@components/grid';
@@ -7,12 +8,53 @@ import { Link } from 'react-router-dom';
 import routePaths from '@constants/routePaths.ts';
 import HuntingListItem from '@pages/hunting/HuntingListItem';
 import { BugReport } from '@/types/bug-report';
+import { getBugReportList } from '@/api/bugReports';
 
-interface CatchRequestListProps {
-  requests: BugReport[];
-}
+function CatchRequestList() {
+  const [requests, setRequests] = useState<BugReport[]>([]);
+  const [latitude, setLatitude] = useState<number | null>(null);
+  const [longitude, setLongitude] = useState<number | null>(null);
 
-function CatchRequestList({ requests }: CatchRequestListProps) {
+  useEffect(() => {
+    const fetchLocation = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            setLatitude(position.coords.latitude);
+            setLongitude(position.coords.longitude);
+          },
+          (error) => {
+            console.error('Error getting location:', error);
+          },
+          { enableHighAccuracy: true },
+        );
+      } else {
+        console.error('Geolocation is not supported by this browser.');
+      }
+    };
+
+    fetchLocation();
+  }, []);
+
+  useEffect(() => {
+    const fetchRequests = async () => {
+      if (latitude !== null && longitude !== null) {
+        try {
+          const responsesInfo = await getBugReportList({
+            currentLatitude: latitude,
+            currentLongitude: longitude,
+          });
+
+          setRequests(responsesInfo.bug_reports);
+        } catch (error) {
+          console.error('Error fetching bug report list:', error);
+        }
+      }
+    };
+
+    fetchRequests();
+  }, [latitude, longitude]);
+
   return (
     <DefaultPaddedContainer>
       <Container direction="column">
