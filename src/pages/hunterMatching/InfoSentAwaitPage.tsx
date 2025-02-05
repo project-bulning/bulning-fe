d5195d4ac0b356cd5d409026fc4ab99d318bcdf5
mini-apple <suc2150@gmail.com>
2025-02-05 16:09:57 +0900

    fix: 헌터지원 후 대기페이지 수정

--- src/pages/hunterMatching/InfoSentAwaitPage.tsx
+++ src/pages/hunterMatching/InfoSentAwaitPage.tsx
@@ -1,28 +1,25 @@
 import { DefaultPaddedContainer } from '@components/container/variants';
 import Container from '@components/container';
-import { Heading, Paragraph } from '@components/text';
+import { Heading } from '@components/text';
 import Button from '@components/button';
-import { useTheme } from '@emotion/react';
 import { useNavigate } from 'react-router-dom';
 import routePaths from '@constants/routePaths.ts';
 
 function InfoSentAwaitPage() {
-  const theme = useTheme();
   const navigate = useNavigate();
 
   const handleBtnClick = () => {
-    navigate(routePaths.HUNTER_APPROVAL);
+    navigate(routePaths.MAIN);
   };
 
   return (
     <DefaultPaddedContainer>
       <Container direction="column" justify="space-between" padding="96px 0 10px 0" height="100dvh">
-        <Container direction="column" gap="5px">
-          <Heading.H3_5 weight="medium">헬피에게 정보를 보냈어요</Heading.H3_5>
-          <Heading.H3_5 weight="medium">이 화면에서 잠시만 기다려주세요</Heading.H3_5>
+        <Container direction="column" gap="10px">
+          <Heading.H4 weight="medium">헬피에게 정보를 보냈어요</Heading.H4>
+          <Heading.H4 weight="medium">매칭이 완료되면 알림을 보내드려요</Heading.H4>
         </Container>
         <Container direction="column" align="center" gap="13px">
-          <Paragraph css={{ fontSize: '12px', color: `${theme.colors.text.subtle}` }}>헬피가 사냥에 동의하면 버튼이 활성화돼요</Paragraph>
           <Button onClick={handleBtnClick}>확인</Button>
         </Container>
       </Container>
