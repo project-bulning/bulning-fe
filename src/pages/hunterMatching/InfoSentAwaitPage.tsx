ee0dbb391ff9b14f6641b1d0a12e8c17371cad56
harugi7 <gimme2721@gmail.com>
2025-02-05 10:31:38 +0900

    feat: 라우팅 수정
    # Conflicts:
    #	src/features/hunterMatching/MoveToQuickChatBottomSheet.tsx

--- src/pages/hunterMatching/InfoSentAwaitPage.tsx
+++ src/pages/hunterMatching/InfoSentAwaitPage.tsx
@@ -1,13 +1,11 @@
 import { DefaultPaddedContainer } from '@components/container/variants';
 import Container from '@components/container';
-import { Heading, Paragraph } from '@components/text';
+import { Heading } from '@components/text';
 import Button from '@components/button';
-import { useTheme } from '@emotion/react';
 import { useNavigate } from 'react-router-dom';
 import routePaths from '@constants/routePaths.ts';
 
 function InfoSentAwaitPage() {
-  const theme = useTheme();
   const navigate = useNavigate();
 
   const handleBtnClick = () => {
@@ -18,11 +16,10 @@ function InfoSentAwaitPage() {
     <DefaultPaddedContainer>
       <Container direction="column" justify="space-between" padding="96px 0 10px 0" height="100dvh">
         <Container direction="column" gap="5px">
-          <Heading.H3_5 weight="medium">헬피에게 정보를 보냈어요</Heading.H3_5>
+          <Heading.H3_5 weight="medium">정보를 보냈어요</Heading.H3_5>
           <Heading.H3_5 weight="medium">이 화면에서 잠시만 기다려주세요</Heading.H3_5>
         </Container>
         <Container direction="column" align="center" gap="13px">
-          <Paragraph css={{ fontSize: '12px', color: `${theme.colors.text.subtle}` }}>헬피가 사냥에 동의하면 버튼이 활성화돼요</Paragraph>
           <Button onClick={handleBtnClick}>확인</Button>
         </Container>
       </Container>
