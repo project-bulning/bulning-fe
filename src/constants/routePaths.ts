92e3771ded22a2e4f75776645500af190d90ba90
mini-apple <suc2150@gmail.com>
2025-02-05 22:09:15 +0900

    feat: 알림 페이지 라우팅

--- src/constants/routePaths.ts
+++ src/constants/routePaths.ts
@@ -15,6 +15,7 @@ const routePaths = {
   DEAL_PROCESS: '/deal-process',
   REVIEW: '/review/:matchId',
   CHAT: '/chat',
+  NOTIFICATION: '/notification',
 };
 
 export default routePaths;
---
+++ src/pages/notification/NotificationPage.tsx
@@ -0,0 +1,9 @@
+import { DefaultPaddedContainer } from '@components/container/variants';
+
+function NotificationPage() {
+  return (
+    <DefaultPaddedContainer>알림페이지</DefaultPaddedContainer>
+  );
+}
+
+export default NotificationPage;
